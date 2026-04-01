3469f36c13a7d9396e1f4ff8eeacfa7e5fed9e95
blanksoul12-ai <blanksoul12@gmail.com>
2026-04-01 22:56:59 +0800

    Refactor district and area filtering logic

--- main.py
+++ main.py
@@ -35,53 +35,53 @@ try:
 	#print(soup.prettify())
 	region_link = soup.find_all('a')
 	for region_tag in region_link:
-
-  	aaa = "District" in region_tag.get('href')
-  	if aaa == True:
-    	request_html = requests.get(region_tag.get('href'))
-    	soup_1 = BeautifulSoup(request_html.text, "html.parser")
-    	district_link = soup_1.find_all('a')
-    	for district_tag in district_link:
-      		bbb = "areas=" in district_tag.get('href')
-      		if bbb == True:
-        		request_html = requests.get(district_tag.get('href'))
-        		soup_2 = BeautifulSoup(request_html.text, "html.parser")
-        		area_link = soup_2.find_all('a')
-        		for area_tag in area_link:
-          			ccc = 'FindBlock' in area_tag.get('href')
-          			if ccc == True:
-
-            			region_text = 'areas=NT' in region_tag.get('href')
-            			if region_text == True:
-              				filter_arr.append(area_tag.get('href'))
-              				filter_arr1.append('New Territories,新界' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
-
-            			region_text = 'areas=IS' in region_tag.get('href')
-            			if region_text == True:
-              				discovery_text = 'Discovery Bay' in area_tag.get_text()
-              				if discovery_text == True:
-                				request_html = requests.get(area_tag.get('href'))
-                				soup_3 = BeautifulSoup(request_html.text, "html.parser")
-                				discovery_link = soup_3.find_all('a')
-                				for discovery_tag in discovery_link:
-                  					instr_discovery = 'Discovery%20Bay' in discovery_tag.get('href')
-                  					if instr_discovery == True:
-                    					filter_arr.append(discovery_tag.get('href'))
-                    					filter_arr1.append(district_tag.get_text() + ' ' + area_tag.get_text() + ' ' + discovery_tag.get_text())
-
-              				if discovery_text == False:
-                				filter_arr.append(area_tag.get('href'))
-                				filter_arr1.append(district_tag.get_text() + ' ' + area_tag.get_text())
-
-            			region_text = 'areas=KL' in region_tag.get('href')
-            			if region_text == True:
-              				filter_arr.append(area_tag.get('href'))
-              				filter_arr1.append('Kowloon,九龍' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
-
-            			region_text = 'areas=HK' in region_tag.get('href')
-            			if region_text == True:
-              				filter_arr.append(area_tag.get('href'))
-              				filter_arr1.append('Hong Kong,香港' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
+	
+		aaa = "District" in region_tag.get('href')
+	  	if aaa == True:
+	    	request_html = requests.get(region_tag.get('href'))
+	    	soup_1 = BeautifulSoup(request_html.text, "html.parser")
+	    	district_link = soup_1.find_all('a')
+	    	for district_tag in district_link:
+	      		bbb = "areas=" in district_tag.get('href')
+	      		if bbb == True:
+	        		request_html = requests.get(district_tag.get('href'))
+	        		soup_2 = BeautifulSoup(request_html.text, "html.parser")
+	        		area_link = soup_2.find_all('a')
+	        		for area_tag in area_link:
+	          			ccc = 'FindBlock' in area_tag.get('href')
+	          			if ccc == True:
+	
+	            			region_text = 'areas=NT' in region_tag.get('href')
+	            			if region_text == True:
+	              				filter_arr.append(area_tag.get('href'))
+	              				filter_arr1.append('New Territories,新界' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
+	
+	            			region_text = 'areas=IS' in region_tag.get('href')
+	            			if region_text == True:
+	              				discovery_text = 'Discovery Bay' in area_tag.get_text()
+	              				if discovery_text == True:
+	                				request_html = requests.get(area_tag.get('href'))
+	                				soup_3 = BeautifulSoup(request_html.text, "html.parser")
+	                				discovery_link = soup_3.find_all('a')
+	                				for discovery_tag in discovery_link:
+	                  					instr_discovery = 'Discovery%20Bay' in discovery_tag.get('href')
+	                  					if instr_discovery == True:
+	                    					filter_arr.append(discovery_tag.get('href'))
+	                    					filter_arr1.append(district_tag.get_text() + ' ' + area_tag.get_text() + ' ' + discovery_tag.get_text())
+	
+	              				if discovery_text == False:
+	                				filter_arr.append(area_tag.get('href'))
+	                				filter_arr1.append(district_tag.get_text() + ' ' + area_tag.get_text())
+	
+	            			region_text = 'areas=KL' in region_tag.get('href')
+	            			if region_text == True:
+	              				filter_arr.append(area_tag.get('href'))
+	              				filter_arr1.append('Kowloon,九龍' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
+	
+	            			region_text = 'areas=HK' in region_tag.get('href')
+	            			if region_text == True:
+	              				filter_arr.append(area_tag.get('href'))
+	              				filter_arr1.append('Hong Kong,香港' + ' ' + district_tag.get_text() + ' ' + area_tag.get_text())
 
 	# --- 3. 資料處理與寫入 Google Sheets --- 
 	df = pd.DataFrame({'Estate Name': pd.Series(filter_arr1), 'Hypelink': pd.Series(filter_arr)}) 
